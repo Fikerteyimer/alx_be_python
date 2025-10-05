aa8c81f3ea06ab4c1ee032c907be573a5105f703
Fikerte Yimer <fikerteyimer06gmail.com>
2025-10-05 22:31:32 +0300

    Add unit tests for SimpleCalculator class

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,33 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    """Unit tests for SimpleCalculator class."""
+
+    def setUp(self):
+        """Create a new calculator instance before each test."""
+        self.calc = SimpleCalculator()
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-5, -7), -12)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-3, -7), 4)
+        self.assertEqual(self.calc.subtract(7, 7), 0)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+        self.assertEqual(self.calc.multiply(-4, -5), 20)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-10, 2), -5)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+        self.assertEqual(self.calc.divide(5, 0), None) 
+        self.assertEqual(self.calc.divide(-6, -2), 3)
+
+if __name__ == "__main__":
+    unittest.main()
